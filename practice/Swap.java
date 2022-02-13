85adbd607397667cc9d64c7aa9b5f1d5ae1966c8
Sumrit Grover <sumritgrover1@gmail.com>
2022-02-13 23:54:58 +0530

    n queens

--- practice/Swap.java
+++ practice/Swap.java
@@ -1,13 +1,74 @@
-import java.util.Scanner;
+// Constraint satsifaction problem
+// N-Queens Problem
 
-public class Swap{
-    public static void main(String [] args){
-        swap(10,20);
+public class Exp3 {
+    final int N = 4;
+
+    void solution(int board[][]) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+                System.out.print(" " + board[i][j]
+                        + " ");
+            System.out.println();
+        }
     }
-    static void swap(int a,int b){
-        int temp=a;
-        a=b;
-        b=temp;
-        System.out.println(a+"  " +b);
+
+    boolean safe(int board[][], int row, int col) {
+        int i, j;
+
+        for (i = 0; i < col; i++)
+            if (board[row][i] == 1)
+                return false;
+
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        for (i = row, j = col; j >= 0 && i < N; i++, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        return true;
+    }
+
+    boolean util(int board[][], int col) {
+
+        if (col >= N)
+            return true;
+
+        for (int i = 0; i < N; i++) {
+
+            if (safe(board, i, col)) {
+
+                board[i][col] = 1;
+
+                if (util(board, col + 1) == true)
+                    return true;
+
+                board[i][col] = 0; // BACKTRACK
+            }
+        }
+
+        return false;
+    }
+
+    boolean solveNQ() {
+        int board[][] = { { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 } };
+
+        if (util(board, 0) == false) {
+            System.out.print("Solution does not exist");
+            return false;
+        }
+
+        solution(board);
+        return true;
+    }
+
+    public static void main(String args[]) {
+        Exp3 Queen = new Exp3();
+        Queen.solveNQ();
     }
 }
